97a4a5b8d09ade0ceb2091211744cf8beb946322
Andrea-Vannini <andreavannini1@gmail.com>
2021-12-16 17:46:23 +0100

    Tango6.1

--- rango/urls.py
+++ rango/urls.py
@@ -3,4 +3,7 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 
 def index(request):
-    context_dict = {
-        'boldmessage': "This message is in bold font thanks to templates!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    urlpatterns = [
+        path('', views.index, name='index'),
+        path('about/', views.about, name='about'),
+        path('category/<slug:category_name_slug>/',
+             views.show_category, name='show_category'),
+    ]
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)

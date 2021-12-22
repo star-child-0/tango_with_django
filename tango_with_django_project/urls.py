be87a1606790b332aa4d39ea29b98d8d31b7473d
Andrea-Vannini <andreavannini1@gmail.com>
2021-12-22 10:00:18 +0100

    Tango11

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-    path('accouts/register/', MyRegistrationViesw.as_view(),
+    path('accouts/register/', MyRegistrationViews.as_view(),
          name='registration_register'),
     path('accounts/', include('registration.backends.simple,urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

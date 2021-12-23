88efdfea169a2a5d30b5442b894a56188114fa94
Andrea-Vannini <andreavannini1@gmail.com>
2021-12-23 12:11:37 +0100

    Tango15

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,8 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
 from datetime import datetime
 from rango.bing_search import run_query, read_bing_key
+from django.contrib.auth.decorators import login_required
+from django import HttpResponse
 
 
 def index(request):
@@ -112,3 +114,60 @@ def search(request):
         if query:
             result_list = run_query(query)
     return render(request, 'rango/search.html', {'result_list': result_list})
+
+
+@login_required
+def like_category(request):
+    cat_id = None
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+        likes = 0
+    if cat_id:
+        cat = Category.objects.get(id=int(cat_id))
+        if cat:
+            likes = cat.likes + 1
+            cat.likes = likes
+            cat.save()
+    return HttpResponse(likes)
+
+
+def get_category_list(max_results=0, starts_with=''):
+    cat_list = []
+    if starts_with:
+        cat_list = Category.objects.filter(name__istartswith=starts_with)
+
+    if max_results > 0:
+        if len(cat_list) > max_results:
+            cat_list = cat_list[:max_results]
+    return cat_list
+
+
+def suggest_category(request):
+    cat_list = []
+    starts_with = ''
+
+    if request.method == 'GET':
+        starts_with = request.GET['suggestion']
+
+    cat_list = get_category_list(8, starts_with)
+    if len(cat_list) == 0:
+        cat_list = Category.objects.order_by('-likes')
+
+
+@login_required
+def auto_add_page(request):
+    cat_id = None
+    url = None
+    titl = None
+    context_dict = {}
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+        url = request.GET['URL']
+        title = request.GET['title']
+        if cat_id:
+            category = Category.objects.get(id=int(cat_id))
+            p = Page.objects.get_or_create(
+                category=category, title=title, url=url)
+            pages = Page.objects.filter(category=category).order_by('-views')
+            context_dict['pages'] = pages
+    return render(request, 'rango/page_list.html')
